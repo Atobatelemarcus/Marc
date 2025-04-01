81f9a31ad25e676dc91513830af0bdccab7e8293
Atobatelemarcus <marcatobz3@gmail.com>
2025-04-01 19:36:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("openBtn").addEventListener("click", function() {
+    document.getElementById("sidebar").style.width = "250px";
+});
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0"; } 
